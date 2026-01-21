9539bcccc824aa3fdf21ee1c4c948984da90df18
Yash Tekale <123358773+yashtekale06@users.noreply.github.com>
2026-01-21 19:34:19 +0530

    Add files via upload

---
+++ Sample_Code.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a,b,c;
+    // a=printf("enter your number:");
+    // scanf("%d",&a);
+    // b=printf("enter your second number:");
+    // scanf("%d",&b);
+    // c=a+b;
+    // printf("addition of two number: %d",c);
+
+    // int num1,num2,sum;
+    // printf("enter 2 number");
+    // scanf("%d ,%d",&num1,&num2);
+    // sum=num1+num2;
+    // printf("addition is %d",sum);
+    
+    // return 0;
+
+    //**********************************/
+    //  name : Yash Tekale
+    // exmaple: different data types
+
+    // *********************************
+    // char name;
+    // int pincode;
+    // float height;
+    // double latitude,longitiude;
+    // name=printf("enter your name: ");
+    // scanf("%c",&name);
+    // pincode=printf("enter your pincode ");
+    // scanf("%d",&pincode);
+    // height=printf("enter your height ");
+    // scanf("%f",&height);
+    // printf("enter your latitude & longitude: ");
+    // scanf("%lf %lf",&latitude,&longitiude);
+    // printf("name is %c\n",name);
+    // printf("pincode is %d\n",pincode);
+    // printf("name is %f\n",height);
+    // printf("location is :%lf,%lf",latitude,longitiude);
+    // return 0;
+
+    //**********************************/
+    //  name : Yash Tekale
+    // exmaple: avrage of 3 float numeber 
+
+    // *********************************
+    // float a,b,c,avg;
+    // printf( "enter 3 float value: ");
+    // scanf("%f %f %f",&a,&b,&c);
+    // avg=(a+b+c)/3;
+    // printf("Avrage of 3 float value: %.2f",avg);
+    /**********************************/
+    //  name : Yash Tekale
+    // exmaple: avrage of 3 float numeber 
+
+    // *********************************
+    float a, c;
+    printf("enter your value: ");
+    scanf("%f",&a);
+    c=(a - 32)*5/9;
+    printf("valu of %f °C",c);
+    return 0;
+}

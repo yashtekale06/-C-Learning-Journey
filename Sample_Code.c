89168cdae73b44d5c94c91a9f4e8524026cb7d0d
Yash Tekale <123358773+yashtekale06@users.noreply.github.com>
2026-01-28 22:16:38 +0530

    Update Sample_Code.c

--- Sample_Code.c
+++ Sample_Code.c
@@ -2,64 +2,60 @@
 
 int main()
 {
-    // int a,b,c;
-    // a=printf("enter your number:");
-    // scanf("%d",&a);
-    // b=printf("enter your second number:");
-    // scanf("%d",&b);
-    // c=a+b;
-    // printf("addition of two number: %d",c);
+    int a,b,c;
+    a=printf("enter your number:");
+    scanf("%d",&a);
+    b=printf("enter your second number:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("addition of two number: %d",c);
 
-    // int num1,num2,sum;
-    // printf("enter 2 number");
-    // scanf("%d ,%d",&num1,&num2);
-    // sum=num1+num2;
-    // printf("addition is %d",sum);
-    
-    // return 0;
-
+    int num1,num2,sum;
+    printf("enter 2 number");
+    scanf("%d ,%d",&num1,&num2);
+    sum=num1+num2;
+    printf("addition is %d",sum);
     //**********************************/
     //  name : Yash Tekale
     // exmaple: different data types
 
     // *********************************
-    // char name;
-    // int pincode;
-    // float height;
-    // double latitude,longitiude;
-    // name=printf("enter your name: ");
-    // scanf("%c",&name);
-    // pincode=printf("enter your pincode ");
-    // scanf("%d",&pincode);
-    // height=printf("enter your height ");
-    // scanf("%f",&height);
-    // printf("enter your latitude & longitude: ");
-    // scanf("%lf %lf",&latitude,&longitiude);
-    // printf("name is %c\n",name);
-    // printf("pincode is %d\n",pincode);
-    // printf("name is %f\n",height);
-    // printf("location is :%lf,%lf",latitude,longitiude);
-    // return 0;
-
+    char name;
+    int pincode;
+    float height;
+    double latitude,longitiude;
+    name=printf("enter your name: ");
+    scanf("%c",&name);
+    pincode=printf("enter your pincode ");
+    scanf("%d",&pincode);
+    height=printf("enter your height ");
+    scanf("%f",&height);
+    printf("enter your latitude & longitude: ");
+    scanf("%lf %lf",&latitude,&longitiude);
+    printf("name is %c\n",name);
+    printf("pincode is %d\n",pincode);
+    printf("height is %f\n",height);
+    printf("location is :%lf,%lf",latitude,longitiude);
     //**********************************/
     //  name : Yash Tekale
     // exmaple: avrage of 3 float numeber 
 
     // *********************************
-    // float a,b,c,avg;
-    // printf( "enter 3 float value: ");
-    // scanf("%f %f %f",&a,&b,&c);
-    // avg=(a+b+c)/3;
-    // printf("Avrage of 3 float value: %.2f",avg);
+    float d,e,f,avg;
+    printf( "enter 3 float value: ");
+    scanf("%f %f %f",&d,&e,&f);
+    avg=(d+e+f)/3;
+    printf("Avrage of 3 float value: %.2f",avg);
     /**********************************/
     //  name : Yash Tekale
-    // exmaple: avrage of 3 float numeber 
+    // exmaple:  temperature from Fahrenheit to Celsius
 
     // *********************************
-    float a, c;
-    printf("enter your value: ");
-    scanf("%f",&a);
-    c=(a - 32)*5/9;
+    float g, Celsius;
+    printf("enter your value : ");
+    scanf("%f",&g);
+    Celsius=(g - 32)*5/9;
     printf("valu of %f °C",c);
     return 0;
 }
+
